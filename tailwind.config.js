a4c2ca67805a0c194dab8d2a399513f14c6e0855
chajaesik01 <ckwotlr@dongguk.edu>
2024-12-31 18:50:13 +0900

    chore: design token 정의 (#9)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,56 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        'hover-primary': 'var(--color-hover-primary)',
+        secondary: 'var(--color-secondary)',
+        'hover-secondary': 'var(--color-hover-secondary)',
+        tertiary: 'var(--color-tertiary)',
+        'hover-tertiary': 'var(--color-hover-tertiary)',
+        danger: 'var(--color-danger)',
+        'hover-danger': 'var(--color-hover-danger)',
+        success: 'var(--color-success)',
+        'hover-success': 'var(--color-hover-success)',
+        neutral: 'var(--color-neutral)',
+        'neutral-variant': 'var(--color-neutral-variant)',
+      },
+      textColor: {
+        dark: 'var(--color-text-dark)',
+        light: 'var(--color-text-light)',
+        gray: 'var(--color-text-gray)',
+      },
+      fontSize: {
+        xlarge: 'var(--font-xlarge)',
+        large: 'var(--font-large)',
+        medium: 'var(--font-medium)',
+        small: 'var(--font-small)',
+      },
+      fontFamily: {
+        sans: ['var(--font-sans)', 'Arial', 'sans-serif'],
+      },
+      borderRadius: {
+        small: 'var(--border-radius-small)',
+        medium: 'var(--border-radius-medium)',
+        large: 'var(--border-radius-large)',
+        xlarge: 'var(--border-radius-xlarge)',
+      },
+      spacing: {
+        xsmall: 'var(--space-xsmall)',
+        small: 'var(--space-small)',
+        medium: 'var(--space-medium)',
+        large: 'var(--space-large)',
+        xlarge: 'var(--space-xlarge)',
+        xxlarge: 'var(--space-xxlarge)',
+      },
+      backgroundColor: {
+        basic: 'var(--color-background)',
+        light: 'var(--color-background-light)',
+        dark: 'var(--color-background-dark)',
+      },
+    },
   },
   plugins: [],
 };
