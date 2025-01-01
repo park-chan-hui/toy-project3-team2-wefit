aba1c50d8640fd76e3a3cab6f52c570c8e55fa41
bbjbc <jobc4550@gmail.com>
2025-01-01 23:22:44 +0900

    style: outlet과 로고 좌우 패딩 추가

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -8,7 +8,7 @@ const Layout = () => {
     <main className="flex min-h-screen justify-center">
       <section className="relative w-full max-w-container bg-white">
         <LogoHeader />
-        <div className="pb-28">
+        <div className="px-3 pb-28">
           <Outlet />
         </div>
         <NavigationBar />
