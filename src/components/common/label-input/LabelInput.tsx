f03c1c0a26f7ff71c85d71a43af1348d35d43db6
chajaesik01 <ckwotlr@dongguk.edu>
2025-01-02 13:57:36 +0900

    feat: useDebounce hook 추가

--- src/components/common/label-input/LabelInput.tsx
+++ src/components/common/label-input/LabelInput.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, useState } from 'react';
+import { useDebounce } from '@/hooks/usedebounce';
 
 type LabelInputProps = {
   title: string;
@@ -9,6 +10,13 @@ type LabelInputProps = {
 const LabelInput = (props: LabelInputProps) => {
   const { title, placeholder, description } = props;
   const [desc, setDesc] = useState(description);
+
+  const debouncedValue = useDebounce({
+    value: desc,
+    delay: 1000,
+  });
+
+  console.log(debouncedValue); // 삭제 예정
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setDesc(e.target.value);
