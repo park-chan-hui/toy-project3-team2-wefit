319db1036d1f7037e2a936afd64c9e1c306dd5ac
chajaesik01 <ckwotlr@dongguk.edu>
2025-01-02 13:58:19 +0900

    refactor: inputHTMLAttributes 적용

--- src/components/common/label-input/LabelInput.tsx
+++ src/components/common/label-input/LabelInput.tsx
@@ -1,14 +1,14 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, InputHTMLAttributes, useState } from 'react';
 import { useDebounce } from '@/hooks/usedebounce';
 
-type LabelInputProps = {
+type LabelInputProps = InputHTMLAttributes<HTMLInputElement> & {
   title: string;
   placeholder: string;
   description: string;
 };
 
 const LabelInput = (props: LabelInputProps) => {
-  const { title, placeholder, description } = props;
+  const { title, placeholder, description, ...inputProps } = props;
   const [desc, setDesc] = useState(description);
 
   const debouncedValue = useDebounce({
@@ -16,7 +16,7 @@ const LabelInput = (props: LabelInputProps) => {
     delay: 1000,
   });
 
-  console.log(debouncedValue); // 삭제 예정
+  console.log(debouncedValue); //삭제 예정
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setDesc(e.target.value);
@@ -26,6 +26,7 @@ const LabelInput = (props: LabelInputProps) => {
     <div className="mb-2 max-w-container">
       <p className="text-large font-bold">{title}</p>
       <input
+        {...inputProps}
         className="mt-1 w-full rounded-medium border border-black p-2 pl-4 pr-4 focus:!border-primary focus:outline-none"
         placeholder={placeholder}
         value={desc}
