d4830ac03f03e4aae93c9b80c7c0f2b8b88550d6
bbjbc <jobc4550@gmail.com>
2025-01-03 15:23:05 +0900

    fix: 라우터 경로 오탈자 수정

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -8,7 +8,7 @@ const ROUTER_PATH = {
   PLAYLIST_DETAIL: '/playlist/:playlistId',
   VIDEO_ADD: '/video-add',
   VIDEO_DETAIL: '/video/:videoId',
-  VIDEO_EDIT: '/mypage/edit/:videoId',
+  VIDEO_EDIT: '/mypage/video-edit/:videoId',
   AUTHOR_DETAIL: '/author/:userId',
   BOOKMARK: '/bookmark',
   BOOKMARK_CATEGORY_ADD: '/bookmark/category-add',
