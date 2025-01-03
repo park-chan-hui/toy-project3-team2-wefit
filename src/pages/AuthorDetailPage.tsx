792beada2ae768eea2f7a372fd88d72268b6e178
bbjbc <jobc4550@gmail.com>
2025-01-03 17:49:22 +0900

    feat: author 상세 페이지 퍼블리싱 (#32)

--- src/pages/AuthorDetailPage.tsx
+++ src/pages/AuthorDetailPage.tsx
@@ -1,5 +1,47 @@
+import { useParams } from 'react-router-dom';
+
+import Button from '@/components/common/button/Button';
+import VideoList from '@/components/video/VideoList';
+import AuthorProfile from '@/components/author/AuthorProfile';
+import { mockUsers } from '@/mocks/mockUsers';
+import { mockVideos } from '@/mocks/mockVideos';
+
 const AuthorDetailPage = () => {
-  return <h1>Author Detail Page!</h1>;
+  const { userId } = useParams<{ userId: string }>();
+  const author = mockUsers.find(user => user.user_id === userId);
+  const authorVideos = mockVideos.filter(video => video.user_id === userId);
+
+  if (!author) {
+    return (
+      <main className="p-4">
+        <h1>존재하지 않는 유저입니다.</h1>
+      </main>
+    );
+  }
+
+  return (
+    <main className="flex flex-col">
+      <AuthorProfile author={author} authorVideos={authorVideos} />
+      <hr className="my-3" aria-hidden="true" />
+
+      <section aria-label="업로드한 영상 목록">
+        <header className="mb-2 flex items-center justify-between">
+          <h2 className="text-lg font-bold">
+            작성자가 업로드한 영상 ({authorVideos.length}개)
+          </h2>
+          <nav className="flex items-center gap-2">
+            <Button variant="outline" size="small">
+              최신순
+            </Button>
+            <Button variant="outline" size="small">
+              인기순
+            </Button>
+          </nav>
+        </header>
+        <VideoList videos={authorVideos} />
+      </section>
+    </main>
+  );
 };
 
 export default AuthorDetailPage;
