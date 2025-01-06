fa61104afd91afc528ee70db2d210f2be80a19ca
chajaesik01 <ckwotlr@dongguk.edu>
2025-01-06 11:06:53 +0900

    refactor: cn 함수 적용

--- src/components/bookmark/BookmarkStatus.tsx
+++ src/components/bookmark/BookmarkStatus.tsx
@@ -1,6 +1,7 @@
 import { FaStar } from 'react-icons/fa';
 import { FaRegTrashAlt } from 'react-icons/fa';
 import { VideoStatsProps } from '@/types/video';
+import { cn } from '@/utils/cn';
 
 const BookmarkStatus = ({
   is_bookmarked,
@@ -11,7 +12,7 @@ const BookmarkStatus = ({
         <FaRegTrashAlt size={25} className="mr-1 cursor-pointer" />
         <FaStar
           size={25}
-          className={`${is_bookmarked ? 'text-primary' : ''} cursor-pointer`}
+          className={cn('cursor-pointer', is_bookmarked ? 'text-primary' : '')}
         />
       </div>
     </div>
--- src/components/common/simple-profile/SimpleProfile.tsx
+++ src/components/common/simple-profile/SimpleProfile.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { CgProfile } from 'react-icons/cg';
 import { UserProps } from '@/types/user';
+import { cn } from '@/utils/cn';
 
 type SimpleProfileProps = Pick<
   UserProps,
@@ -35,20 +36,22 @@ const SimpleProfile = (userData: SimpleProfileProps) => {
     <Link to={`/author/${userData.user_id}`} className="inline-block">
       <div className="flex items-center gap-1">
         <div
-          className={`flex justify-center overflow-auto rounded-full border border-none`}
+          className={
+            'flex justify-center overflow-auto rounded-full border border-none'
+          }
         >
           {userData.user_image ? (
             <img
               src={userData.user_image}
               alt="프로필사진"
-              className={`object-cover ${imageSizeStyles[imageSize]}`}
+              className={cn('object-cover', imageSizeStyles[imageSize])}
             />
           ) : (
             <CgProfile size={20} />
           )}
         </div>
         <div
-          className={`text-gray-700 font-semibold ${textSizeStyles[textSize]}`}
+          className={cn('font-semibold text-gray', textSizeStyles[textSize])}
         >
           {userData.nickname || '데이터가 없습니다.'}
         </div>
