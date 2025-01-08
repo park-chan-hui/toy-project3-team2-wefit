3dd65c30ca591bcf654b24261199f13cdc589236
chajaesik01 <ckwotlr@dongguk.edu>
2025-01-08 11:36:04 +0900

    feat: 동영상 스와핑 기능 구현 (#60)

--- src/components/playlist/PlayList.tsx
+++ src/components/playlist/PlayList.tsx
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 import { mockUsers } from '@/mocks/mockUsers';
 import type { BookmarkProps } from '@/types/bookmark';
 import PlayListItem from '@/components/playlist/PlayListItem';
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from 'react-beautiful-dnd';
 
 type PlayListProps = {
   bookmark: BookmarkProps;
@@ -14,21 +21,68 @@ const PlayList = ({
   onThumbnailChange,
   thumbnail,
 }: PlayListProps) => {
+  const [videoList, setVideoList] = useState(bookmark.video_list);
+
+  const onDragEnd = (result: DropResult) => {
+    if (!result?.destination) return;
+
+    const sourceIndex = result.source.index;
+    const destinationIndex = result.destination.index;
+    const newList = [...videoList];
+    const pickedItem = newList[sourceIndex];
+
+    newList.splice(sourceIndex, 1);
+    newList.splice(destinationIndex, 0, pickedItem);
+    setVideoList(newList);
+  };
+
+  // 이벤트 버블링 제한
+  const handleClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+  };
+
   return (
-    <div className="flex flex-col gap-4">
-      {bookmark.video_list.map(video => {
-        const userData = mockUsers.find(user => user.user_id === video.user_id);
-        return (
-          <PlayListItem
-            key={video.video_id}
-            video={video}
-            thumbnail={thumbnail}
-            onThumbnailChange={onThumbnailChange}
-            userData={userData}
-          />
-        );
-      })}
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="droppable">
+        {provided => (
+          <div
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            className="flex flex-col gap-4"
+          >
+            {videoList.map((video, index) => {
+              const userData = mockUsers.find(
+                user => user.user_id === video.user_id,
+              );
+              return (
+                <Draggable
+                  key={video.video_id}
+                  draggableId={video.video_id}
+                  index={index}
+                >
+                  {provided => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      onClick={handleClick}
+                    >
+                      <PlayListItem
+                        video={video}
+                        thumbnail={thumbnail}
+                        onThumbnailChange={onThumbnailChange}
+                        userData={userData}
+                      />
+                    </div>
+                  )}
+                </Draggable>
+              );
+            })}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
 
