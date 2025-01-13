f7d1572c3e6b6477bdf2bbb6c3913fe1dca82c6e
chajaesik01 <ckwotlr@dongguk.edu>
2025-01-13 14:43:49 +0900

    feat: 운동할 때 듣기 좋은 플리 supabase 연동 (#91)

--- src/components/playlist/PlayMusicList.tsx
+++ src/components/playlist/PlayMusicList.tsx
@@ -1,29 +1,34 @@
-import { mockPlayLists } from '@/mocks/mockVideos';
+import { useMusics } from '@/hooks/useMusics';
 import { getTimeAgo } from '@/utils/getTimeAgo';
 import { Link } from 'react-router-dom';
 
 const PlayMusicList = () => {
+  const { data: musics, isLoading } = useMusics();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
-      {mockPlayLists.map(playlist => (
-        <Link to={`/playlist/${playlist.list_id}`} key={playlist.list_id}>
+      {musics?.map(music => (
+        <Link to={`/playlist/${music.list_id}`} key={music.list_id}>
           <div className="mb-2">
             <figure className="relative mb-3 aspect-video h-[80%] w-full overflow-hidden rounded-lg">
               <img
-                src={playlist.thumbnail}
-                alt={playlist.title}
+                src={music.thumbnail}
+                alt={music.title}
                 className="h-full w-full object-cover"
               />
             </figure>
             <div className="flex w-full flex-row justify-between gap-1">
               <div className="relative flex w-[65%] flex-row overflow-hidden whitespace-nowrap">
                 <h2 className="overflow-hidden text-ellipsis whitespace-nowrap font-bold">
-                  {playlist.title}
+                  {music.title}
                 </h2>
-                <h2 className="font-bold">({playlist.video_list.length})</h2>
+                <h2 className="font-bold">({music.video_list.length})</h2>
               </div>
               <p className="mr-1 flex items-center text-small">
-                최종 수정일 {getTimeAgo(playlist.created_at)}
+                최종 수정일 {getTimeAgo(music.created_at)}
               </p>
             </div>
           </div>
