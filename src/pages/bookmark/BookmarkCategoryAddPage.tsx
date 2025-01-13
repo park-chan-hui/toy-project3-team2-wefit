6e80dabafb71cbe563b0eb785c218a6f0e9a427f
chajaesik01 <ckwotlr@dongguk.edu>
2025-01-13 12:31:19 +0900

    refactor: 이미지 확장자 검사, schema 분리

--- src/pages/bookmark/BookmarkCategoryAddPage.tsx
+++ src/pages/bookmark/BookmarkCategoryAddPage.tsx
@@ -11,7 +11,7 @@ import { FaRegCircle } from 'react-icons/fa6';
 import { useNavigate } from 'react-router-dom';
 import { toastSuccess, toastError } from '@/utils/toast';
 import { useForm, Controller } from 'react-hook-form';
-import { z } from 'zod';
+import { categorySchema } from '@/schema/categorySchema';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 type CheckedVideos = {
@@ -24,12 +24,6 @@ type CheckInput = {
   videos: string;
 };
 
-const schema = z.object({
-  categoryName: z.string().min(1, { message: '카테고리 명을 입력해주세요' }),
-  imgFile: z.string().min(1, { message: '썸네일을 선택해주세요' }),
-  videos: z.string().min(1, { message: '최소 하나의 동영상을 선택해주세요' }),
-});
-
 const BookmarkCategoryEditPage = () => {
   const [checkedVideos, setCheckedVideos] = useState<CheckedVideos>({});
 
@@ -40,7 +34,7 @@ const BookmarkCategoryEditPage = () => {
     setValue,
     clearErrors,
   } = useForm<CheckInput>({
-    resolver: zodResolver(schema),
+    resolver: zodResolver(categorySchema),
     defaultValues: {
       categoryName: '',
       imgFile: '',
@@ -112,83 +106,96 @@ const BookmarkCategoryEditPage = () => {
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Controller
-          name="categoryName"
-          control={control}
-          render={({ field }) => (
-            <LabelInput
-              title="카테고리 명"
-              placeholder="카테고리 명을 입력해주세요."
-              onChange={e => {
-                field.onChange(e);
-                if (e.target.value) {
-                  clearErrors('categoryName');
-                }
-              }}
-              value={field.value}
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="max-h-[75vh] flex-1 overflow-y-auto">
+        <div className="space-y-3">
+          <div>
+            <Controller
+              name="categoryName"
+              control={control}
+              render={({ field }) => (
+                <LabelInput
+                  title="카테고리 명"
+                  placeholder="카테고리 명을 입력해주세요."
+                  onChange={e => {
+                    field.onChange(e);
+                    if (e.target.value) {
+                      clearErrors('categoryName');
+                    }
+                  }}
+                  value={field.value}
+                />
+              )}
             />
-          )}
-        />
-        {errors.categoryName && (
-          <p className="text-sm text-red-500">{errors.categoryName.message}</p>
-        )}
+            {errors.categoryName && (
+              <p className="text-sm text-red-500">
+                {errors.categoryName.message}
+              </p>
+            )}
+          </div>
 
-        <ThumbnailUpload
-          title="카테고리 썸네일"
-          message="카테고리 썸네일을 추가해보아요!"
-          imgFile={control._formValues.imgFile}
-          onImageChange={handleImageChange}
-          isEditPage={false}
-        />
-        {errors.imgFile && (
-          <p className="text-sm text-red-500">{errors.imgFile.message}</p>
-        )}
+          <div>
+            <ThumbnailUpload
+              title="카테고리 썸네일"
+              message="카테고리 썸네일을 추가해보아요!"
+              imgFile={control._formValues.imgFile}
+              onImageChange={handleImageChange}
+              isEditPage={false}
+            />
+            {errors.imgFile && (
+              <p className="mt-2 text-sm text-red-500">
+                {errors.imgFile.message}
+              </p>
+            )}
+          </div>
 
-        <div className="flex flex-col gap-4">
-          <p className="mt-2 text-base font-bold">카테고리에 넣을 영상</p>
+          <div>
+            <p className="my-2 text-base font-bold">카테고리에 넣을 영상</p>
+            <div className="flex flex-col gap-5">
+              {filteredVideos.map(video => (
+                <div key={video.video_id} className="flex items-center gap-2">
+                  {checkedVideos[video.video_id] ? (
+                    <FaCheckCircle
+                      className="flex-shrink-0 cursor-pointer text-3xl"
+                      onClick={() => handleClick(video.video_id)}
+                    />
+                  ) : (
+                    <FaRegCircle
+                      className="flex-shrink-0 cursor-pointer text-3xl"
+                      onClick={() => handleClick(video.video_id)}
+                    />
+                  )}
 
-          <div className="flex h-[30vh] w-full flex-col gap-5 overflow-y-auto">
-            {filteredVideos.map(video => (
-              <div key={video.video_id} className="flex items-center gap-2">
-                {checkedVideos[video.video_id] ? (
-                  <FaCheckCircle
-                    className="flex-shrink-0 cursor-pointer text-3xl"
+                  <div
+                    className="w-full cursor-pointer overflow-hidden"
                     onClick={() => handleClick(video.video_id)}
-                  />
-                ) : (
-                  <FaRegCircle
-                    className="flex-shrink-0 cursor-pointer text-3xl"
-                    onClick={() => handleClick(video.video_id)}
-                  />
-                )}
-
-                <div
-                  className="w-full cursor-pointer overflow-hidden"
-                  onClick={() => handleClick(video.video_id)}
-                >
-                  <div className="pointer-events-none flex-1 overflow-hidden">
-                    <BookmarkItem {...video} />
+                  >
+                    <div className="pointer-events-none flex-1 overflow-hidden">
+                      <BookmarkItem {...video} />
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
+            {errors.videos && (
+              <p className="my-2 text-sm text-red-500">
+                {errors.videos.message}
+              </p>
+            )}
           </div>
-          {errors.videos && (
-            <p className="text-sm text-red-500">{errors.videos.message}</p>
-          )}
-
-          <Button
-            variant="secondary"
-            className="w-full rounded-medium"
-            type="submit"
-          >
-            해당 카테고리 추가
-          </Button>
         </div>
-      </form>
-    </>
+      </div>
+
+      <div className="sticky bottom-0">
+        <Button
+          variant="secondary"
+          className="my-2 w-full rounded-medium"
+          type="submit"
+        >
+          해당 카테고리 추가
+        </Button>
+      </div>
+    </form>
   );
 };
 
---
+++ src/schema/categorySchema.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+
+const categorySchema = z.object({
+  categoryName: z.string().min(1, { message: '카테고리 명을 입력해주세요' }),
+  imgFile: z
+    .string()
+    .min(1, { message: '썸네일을 선택해주세요' })
+    .refine(
+      value => {
+        console.log(value);
+        const validPrefixes = [
+          'data:image/jpg;base64,',
+          'data:image/jpeg;base64,',
+          'data:image/png;base64,',
+        ];
+
+        return validPrefixes.some(prefix => value.startsWith(prefix));
+      },
+      {
+        message: 'jpg, jpeg, png 파일만 허용됩니다.',
+      },
+    ),
+  videos: z.string().min(1, { message: '최소 하나의 동영상을 선택해주세요' }),
+});
+
+export { categorySchema };
