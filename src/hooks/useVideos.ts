9fc0169794b457aeea7530b5bd1308e8bd6b5134
bbjbc <jobc4550@gmail.com>
2025-01-14 11:52:55 +0900

    feat: 영상 훅에 무한 스크롤 로직 추가 (#97)

--- src/hooks/useVideos.ts
+++ src/hooks/useVideos.ts
@@ -1,4 +1,7 @@
+import { useEffect, useRef } from 'react';
+
 import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { useInView } from 'react-intersection-observer';
 
 import { fetchVideos, fetchVideo, fetchSelectVideos } from '@/api/videos';
 
@@ -6,9 +9,15 @@ interface UseVideosOptions {
   videoId?: string;
   videosId?: string[];
   category?: string;
+  useInfiniteScroll?: boolean;
 }
 
-const useVideos = ({ videoId, videosId, category }: UseVideosOptions = {}) => {
+const useVideos = ({
+  videoId,
+  videosId,
+  category,
+  useInfiniteScroll = true,
+}: UseVideosOptions = {}) => {
   const videosQuery = useInfiniteQuery({
     queryKey: ['videos', category],
     queryFn: ({ pageParam }) => fetchVideos({ pageParam, category }),
@@ -48,6 +57,36 @@ const useVideos = ({ videoId, videosId, category }: UseVideosOptions = {}) => {
     enabled: !!videosId,
   });
 
+  // 무한 스크롤 관련 로직
+  const { ref: infiniteScrollRef, inView } = useInView({
+    threshold: 0.1,
+  });
+
+  const timeoutId = useRef<NodeJS.Timeout>();
+
+  useEffect(() => {
+    if (
+      useInfiniteScroll &&
+      inView &&
+      videosQuery.hasNextPage &&
+      !videosQuery.isFetchingNextPage
+    ) {
+      if (timeoutId.current) {
+        clearTimeout(timeoutId.current);
+      }
+
+      timeoutId.current = setTimeout(() => {
+        videosQuery.fetchNextPage();
+      }, 300);
+    }
+
+    return () => {
+      if (timeoutId.current) {
+        clearTimeout(timeoutId.current);
+      }
+    };
+  }, [inView, videosQuery, useInfiniteScroll]);
+
   // 현재 카테고리의 모든 비디오 데이터
   const allVideos =
     Array.from(
@@ -63,6 +102,7 @@ const useVideos = ({ videoId, videosId, category }: UseVideosOptions = {}) => {
     videoQuery,
     selectVideosQuery,
     allVideos,
+    infiniteScrollRef,
     fetchNextPage: videosQuery.fetchNextPage,
     hasNextPage: videosQuery.hasNextPage,
     isFetchingNextPage: videosQuery.isFetchingNextPage,
