2a95786c906625285229c8cf28287afd5460f3ac
chajaesik01 <ckwotlr@dongguk.edu>
2025-01-21 17:49:10 +0900

    refactor: grid, map적용

--- src/components/skeleton/bookmark/BookmarkPlayListSkeleton.tsx
+++ src/components/skeleton/bookmark/BookmarkPlayListSkeleton.tsx
@@ -1,35 +1,15 @@
 const BookmarkPlayListSkeleton = () => {
   return (
     <section className="flex animate-pulse flex-col">
-      <div className="flex h-[40vh] w-full flex-wrap gap-1">
-        <div className="mb-9 flex w-full gap-4">
-          <div className="flex w-1/2 flex-col gap-2">
+      <div className="grid h-[45vh] w-full grid-cols-2 gap-4">
+        {[...Array(4)].map((_, index) => (
+          <div key={index} className="flex flex-col gap-2">
             <div className="aspect-[2/1] h-24 w-full rounded-md bg-gray-200" />
             <div className="aspect-[2/1] h-4 w-40 rounded-md bg-gray-200" />
             <div className="aspect-[2/1] h-4 w-24 rounded-md bg-gray-200" />
             <div className="aspect-[2/1] h-4 w-24 rounded-md bg-gray-200" />
           </div>
-          <div className="flex w-1/2 flex-col gap-1">
-            <div className="aspect-[2/1] h-24 w-full rounded-md bg-gray-200" />
-            <div className="aspect-[2/1] h-4 w-40 rounded-md bg-gray-200" />
-            <div className="aspect-[2/1] h-4 w-24 rounded-md bg-gray-200" />
-            <div className="aspect-[2/1] h-4 w-24 rounded-md bg-gray-200" />
-          </div>
-        </div>
-        <div className="flex w-full gap-4">
-          <div className="flex w-1/2 flex-col gap-1">
-            <div className="aspect-[2/1] h-24 w-full rounded-md bg-gray-200" />
-            <div className="aspect-[2/1] h-4 w-40 rounded-md bg-gray-200" />
-            <div className="aspect-[2/1] h-4 w-24 rounded-md bg-gray-200" />
-            <div className="aspect-[2/1] h-4 w-24 rounded-md bg-gray-200" />
-          </div>
-          <div className="flex w-1/2 flex-col gap-1">
-            <div className="aspect-[2/1] h-24 w-full rounded-md bg-gray-200" />
-            <div className="aspect-[2/1] h-4 w-40 rounded-md bg-gray-200" />
-            <div className="aspect-[2/1] h-4 w-24 rounded-md bg-gray-200" />
-            <div className="aspect-[2/1] h-4 w-24 rounded-md bg-gray-200" />
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
