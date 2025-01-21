6b44b69d5df69cfc8b6c440f481cd11f771afe10
bbjbc <jobc4550@gmail.com>
2025-01-21 18:54:42 +0900

    style: 북마크 플레이리스트 스켈레톤 스타일 수정

--- src/components/skeleton/bookmark/BookmarkPlayListSkeleton.tsx
+++ src/components/skeleton/bookmark/BookmarkPlayListSkeleton.tsx
@@ -1,13 +1,16 @@
 const BookmarkPlayListSkeleton = () => {
   return (
     <section className="flex animate-pulse flex-col">
-      <div className="grid h-[45vh] w-full grid-cols-2 gap-4">
+      <div className="grid w-full grid-cols-2 gap-4">
         {[...Array(4)].map((_, index) => (
           <div key={index} className="flex flex-col gap-2">
-            <div className="aspect-[2/1] h-24 w-full rounded-md bg-gray-200" />
-            <div className="aspect-[2/1] h-4 w-40 rounded-md bg-gray-200" />
-            <div className="aspect-[2/1] h-4 w-24 rounded-md bg-gray-200" />
-            <div className="aspect-[2/1] h-4 w-24 rounded-md bg-gray-200" />
+            <div className="aspect-video w-full rounded-md bg-gray-200" />
+            <div className="h-5 w-full rounded-md bg-gray-200" />
+            <div className="flex items-center gap-1">
+              <div className="h-6 w-6 rounded-full bg-gray-200" />
+              <div className="h-4 w-24 rounded-md bg-gray-200" />
+            </div>
+            <div className="h-4 w-24 rounded-md bg-gray-200" />
           </div>
         ))}
       </div>
